72daa1f458756592bb252228c8c9b8e120431dc3
Ayla Reis <ayla.reis810@gmail.com>
2019-04-28 23:27:14 -0300

    Resolução exercício 3

--- Resolucao_lista_3.R
+++ Resolucao_lista_3.R
@@ -10,3 +10,22 @@ print(z*09946998408)
 
 #Exercício 3
 head(mtcars)
+
+#3.1
+print(str(mtcars))
+
+#3.2
+dim(mtcars)
+
+#3.3
+print(mtcars[,3])
+
+#3.4
+print(mtcars[2,])
+
+#3.5
+print(mtcars[4,2])
+
+#3.6
+print(summary(mtcars))
+
