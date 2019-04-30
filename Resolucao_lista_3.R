6be57ed0046be51136f26948c12c69105c9e5b93
Ayla Reis <ayla.reis810@gmail.com>
2019-04-30 13:14:46 -0300

    Resolução exercício 4

--- Resolucao_lista_3.R
+++ Resolucao_lista_3.R
@@ -29,3 +29,21 @@ print(mtcars[4,2])
 #3.6
 print(summary(mtcars))
 
+
+#Exercício 4
+install.packages("ffbase", dependencies = TRUE)
+require(ffbase)
+setwd("/Users/reis/Desktop/AYLA/UFPE/analise_de_dados_pos_graduacao_davi_moreira/AD_lista_3/")
+turmas_ne <- read.csv2.ffdf(file = "TURMAS.csv", sep = "|")
+setwd("/Users/reis/Desktop/AYLA/UFPE/analise_de_dados_pos_graduacao_davi_moreira/AD_lista_3")
+save.ffdf(turmas_ne, dir = "/Users/reis/Desktop/AYLA/UFPE/analise_de_dados_pos_graduacao_davi_moreira/AD_lista_3/turmas_ne", overwrite = TRUE)
+rm(list = ls())
+
+setwd ("/Users/reis/Desktop/AYLA/UFPE/analise_de_dados_pos_graduacao_davi_moreira/AD_lista_3/")
+load.ffdf(dir="turmas_ne/")
+turmas_pe <- subset(turmas_ne, CO_UF == 26)
+dim(turmas_pe)
+
+turmas_pe <- as.data.frame(turmas_ne)
+setwd("/Users/reis/Desktop/AYLA/UFPE/analise_de_dados_pos_graduacao_davi_moreira/AD_lista_3/")
+save (turmas_pe, file = "turmas_pe_censo_escolar_2016.RData")
